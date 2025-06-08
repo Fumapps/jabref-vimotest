18fc942816fd1dac8dbac4294fc6a02ff0a4d62d
Mario Fuksa <44242539+fumapps@users.noreply.github.com>
2025-06-08 21:13:31 +0200

    ViMoTest: fix compile errors

---
+++ src/test/java/ViMoTests/MainTableTests/JabRefMainTableViewModelTestSetupImpl.java
@@ -0,0 +1,18 @@
+package ViMoTests.MainTableTests;
+
+public class JabRefMainTableViewModelTestSetupImpl extends JabRefMainTableViewModelTestSetup {
+    @Override
+    public void Init() {
+
+    }
+
+    @Override
+    public void SetBibFileTestContext() {
+
+    }
+
+    @Override
+    public JabRefMainTableViewModel BuildSut() {
+        return null;
+    }
+}
